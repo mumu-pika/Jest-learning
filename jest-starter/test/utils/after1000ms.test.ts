18e3c2d37aea6637cccb2bd812f8207bc6e6d02c
mumu <rmli1912@foxmail.com>
2022-12-07 14:54:24 +0800

    update mock logger

--- jest-starter/test/utils/after1000ms.test.ts
+++ jest-starter/test/utils/after1000ms.test.ts
@@ -13,7 +13,7 @@ import after1000ms from "utils/after1000ms"
 //   })
 // })
 
-/* 
+/*
   使用jest.fn 生成一个监听函数, 然后马上断言这个函数
   是没有被调用过的; 之后, 在调用after1000ms之后, 用
   jest.runAllTimers快进时间, 最后来判断callback是否
@@ -35,5 +35,11 @@ describe('after1000ms', () => {
     after1000ms(callback)
 
     jest.runAllTimers() // 快进时间
+
+    expect(callback).toHaveBeenCalled()
+    expect(setTimeout).toHaveBeenCalledTimes(1) // 期望调用过一次setTimeout
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000)
+
+    
   })
 })
